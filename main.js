d3998bfbf9a2d52f8b36eb7541cf2f4281245aad
Siddhartha Konduru <kondurus@usf.edu>
2024-12-16 15:21:51 -0500

    calculateButton

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 function calculateTip(event) {
     event.preventDefault();
-    const billAmount = event.target.form.billAmount.value;
-    const tipPercentage = event.target.form.tipPercentage.value;
+    const form = event.target;
+    const billAmount = form.billAmount.value;
+    const tipPercentage = form.tipPercentage.value;
 
-    if (!billAmount || billAmount <= 0 || !tipPercentage || tipPercentage <= 0) {
+    if (billAmount <= 0 || tipPercentage <= 0) {
         alert("Please enter valid values.");
         return;
     }
 
-    const tipAmount = (billAmount * tipPercentage) / 100;
-    const totalAmount = parseFloat(billAmount) + tipAmount;
+    const tipAmount = billAmount * (tipPercentage / 100);
+    const totalAmount = Number(billAmount) + tipAmount;
 
-    const resultDiv = document.getElementById("result");
-    resultDiv.innerHTML = `
+    form.querySelector("#result").innerHTML = `
         Tip: $${tipAmount.toFixed(2)}<br>
-        Total Amount: $${totalAmount.toFixed(2)}
+        Total: $${totalAmount.toFixed(2)}
     `;
 }
+
+document.querySelector("#calculatorForm").addEventListener("submit", calculateTip);
