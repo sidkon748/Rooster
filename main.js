86e35e18cd20654f103c47967f08264312074a66
Siddhartha Konduru <kondurus@usf.edu>
2024-12-16 15:16:03 -0500

    Create  function calculateTip

---
+++ main.js
@@ -0,0 +1,7 @@
+function calculateTip(event) {
+    event.preventDefault();
+    const billAmount = event.target.form.billAmount.value;
+    const tipPercentage = event.target.form.tipPercentage.value;
+
+
+}
